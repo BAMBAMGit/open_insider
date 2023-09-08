a8a2ebeb1d4b94a1dd3a6cf5976a6b960f32fdcd
George <gmittendorf@gmail.com>
2023-09-08 00:55:03 -0700

    fixes

--- api/index.py
+++ api/index.py
@@ -10,5 +10,5 @@ def home():
     # return "hello world"
     return render_template('index.html', result=result, mydate=mydate, df=df)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
