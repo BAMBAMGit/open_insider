a6d9796bd34684112dfe3ae1d6b83a6d0c0b138c
George <gmittendorf@gmail.com>
2023-09-09 23:42:44 -0700

    hello world trial

--- api/index.py
+++ api/index.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
-from api.myscript import result, mydate, df
+# from flask import Flask, render_template
+# from api.myscript import result, mydate, df
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     # Your Python code goes here
+    
+#     # return "hello world"
+#     return render_template('index.html', result=result, mydate=mydate, df=df)
+
+# if __name__ == '__main__':
+#     app.run()
+#     # app.run(debug=True)
+
+
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Your Python code goes here
-    
-    # return "hello world"
-    return render_template('index.html', result=result, mydate=mydate, df=df)
+    return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run()
-    # app.run(debug=True)
+@app.route('/about')
+def about():
+    return 'About'
