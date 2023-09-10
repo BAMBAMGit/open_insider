c35f11bbb00cebfe19ea1910bb5b7bbc6a801a6e
George <gmittendorf@gmail.com>
2023-09-10 01:02:49 -0700

    query_params

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from api.datetime_helper import date_reformatted_start
-from api.scrape_and_store_df import table_html
+from api.build_query_params import query_params
+# from api.scrape_and_store_df import table_html
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ def home():
     
     # return "hello world"
 
-    return render_template('index.html', result="hello world!!!", mydate=date_reformatted_start, df='table_html')
+    return render_template('index.html', result="hello world!!!", mydate=date_reformatted_start, df = query_params)  #, df='table_html')
 
 # if __name__ == '__main__':
 #     app.run()
