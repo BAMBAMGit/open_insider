c38c0ee76f5660df8d0160c3f3feae4040189885
George <gmittendorf@gmail.com>
2023-09-09 23:20:15 -0700

    testing

--- api/myscript.py
+++ api/myscript.py
@@ -7,6 +7,10 @@ from api.scrape_and_store_df import table_html
 # scrape_openinsider_and_return_df(url)
 
 result = "Hello, World!"  # Replace this with your Python code logic
+
+mydate = result
+df = result
+
 # mydate = date_reformatted_start
 # df = table_html
 # print(df)
