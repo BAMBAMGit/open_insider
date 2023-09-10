dd1fac5d0e6a9a42cea7c277b3341d501d83c956
George <gmittendorf@gmail.com>
2023-09-09 23:48:54 -0700

    revert back from hello world

--- api/index.py
+++ api/index.py
@@ -1,28 +1,15 @@
-# from flask import Flask, render_template
-# from api.myscript import result, mydate, df
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     # Your Python code goes here
-    
-#     # return "hello world"
-#     return render_template('index.html', result=result, mydate=mydate, df=df)
-
-# if __name__ == '__main__':
-#     app.run()
-#     # app.run(debug=True)
-
-
-from flask import Flask
+from flask import Flask, render_template
+from api.myscript import result, mydate, df
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    # Your Python code goes here
+    
+    # return "hello world"
+    return render_template('index.html', result=result, mydate=mydate, df=df)
 
-@app.route('/about')
-def about():
-    return 'About'
+if __name__ == '__main__':
+    app.run()
+    # app.run(debug=True)
