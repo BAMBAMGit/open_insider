2fc01f0b9043b188f68e2b6c23749f348701bb18
George <gmittendorf@gmail.com>
2023-09-10 00:54:11 -0700

    trying the df = tablehtml

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from api.datetime_helper import date_reformatted_start
-# from api.scrape_and_store_df import table_html
+from api.scrape_and_store_df import table_html
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ def home():
     
     # return "hello world"
 
-    return render_template('index.html', result="hello world!!!", mydate=date_reformatted_start)  #, df='table_html')
+    return render_template('index.html', result="hello world!!!", mydate=date_reformatted_start, df='table_html')
 
 # if __name__ == '__main__':
 #     app.run()
